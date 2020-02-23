1e61a1a5c3e6ef12c56052059d275bb4e7e6762c
nhttruc <nhttag@gmail.com>
2020-02-23 15:57:04 +0700

    tính giai thừa

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
+int giaithua(int n){
+    if(n==0)
+        return 1;
+    return n*giaithua(n-1);
+}
 int main(int argc, char const *argv[]){
-    
-    return 0;
+    int a;
+    cout<<"Nhập n = ";
+    cin>>a;
+    int kq=giaithua(a);
+    cout<<a<<"!="<<kq;
+  return 0;
 }
